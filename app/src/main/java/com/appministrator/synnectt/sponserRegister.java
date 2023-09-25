42087ad406b2c7e34a626e28c4fa4ba144e10f03
JeyadeepakUR <jeyadeepak2005@gmail.com>
2023-09-25 14:10:00 +0530

    Backend start

--- app/src/main/java/com/appministrator/synnectt/sponserRegister.java
+++ app/src/main/java/com/appministrator/synnectt/sponserRegister.java
@@ -1,17 +1,39 @@
 package com.appministrator.synnectt;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class sponserRegister extends AppCompatActivity {
     Button regBtn;
-    private EditText SPONNAMEedt, GSTINedt, SPONIDedt,SPONPHedt, ORGIDedt, OrgPwedt, OrgcPwedt;
+    private FirebaseAuth fAuth;
+    FirebaseUser fUser;
+    String fUid;
+    FirebaseFirestore fStore;
+    private DocumentReference mUserDocRef;
+    private EditText SPONNAMEedt, SPONIDedt,SPONPHedt, ORGIDedt, OrgPwedt, OrgcPwedt;
+    String sponname, sponid, sponph, orgid, orgpw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,17 +54,58 @@ public class sponserRegister extends AppCompatActivity {
                 //startActivity(new Intent(sponserRegister.this, MainActivity.class));
                 boolean empty =
                                 SPONNAMEedt.getText().toString().isEmpty() ||
-                                SPONPHedt.getText().toString().isEmpty() ||
-                                SPONIDedt.getText().toString().isEmpty() ||
-                                ORGIDedt.getText().toString().isEmpty()||
-                                GSTINedt.getText().toString().isEmpty();
+                                OrgPwedt.getText().toString().isEmpty() ||
+                                OrgcPwedt.getText().toString().isEmpty() ||
+                                SPONIDedt.getText().toString().isEmpty()||
+                                SPONPHedt.getText().toString().isEmpty()||
+                                SPONIDedt.getText().toString().isEmpty()||
+                                ORGIDedt.getText().toString().isEmpty();
                 if (!empty) {
-                    if(SPONPHedt.getText().length() != 10){
+                    if (SPONPHedt.getText().length() != 10) {
                         SPONPHedt.setError("Enter a 10digit valid phone number");
                         Toast.makeText(sponserRegister.this, "Invalid Phone Number", Toast.LENGTH_SHORT).show();
                         return;
                     }
+                    if (ORGIDedt.getText().length() != 21) {
+                        ORGIDedt.setError("Enter a valid Corporation Identification Number");
+                        Toast.makeText(sponserRegister.this, "Invalid CID", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    sponname = SPONNAMEedt.getText().toString().trim();
+                    sponid = SPONIDedt.getText().toString();
+                    sponph = SPONPHedt.getText().toString().trim();
+                    studPhno = StuPhedt.getText().toString().trim();
+                    studPw = StudPwEdt.getText().toString().trim();
+                    studCpw = StudCpwEdt.getText().toString().trim();
+                    studDept = StuDeptEdt.getText().toString().trim();
 
+                    if(!Objects.equals(studCpw, studPw)) {
+                        Toast.makeText(StudentRegister.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+
+                    fAuth.createUserWithEmailAndPassword(sponid, orgpw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                fUser = task.getResult().getUser();
+                                if (fUser!=null) fUid = fUser.getUid();
+                                mUserDocRef = fStore.collection("users").document(fUid);
+                                Map<String, Object> studInfo = new HashMap<>();
+                                studInfo.put("studName", studName);
+                                studInfo.put("studNetId", studNetId);
+                                studInfo.put("studRegno", studRegno);
+                                studInfo.put("studPhno", studPhno);
+                                studInfo.put("studDept", studDept);
+
+                                mUserDocRef.set(studInfo).addOnSuccessListener(unused -> {
+                                    startActivity(new Intent(StudentRegister.this, MainActivity.class));
+                                    finish();
+                                }).addOnFailureListener(e -> Log.e(TAG, "onFailure: firebase updating failed" + e.getMessage()));
+                            }
+                        }
+                    });
             }
         }
     });
